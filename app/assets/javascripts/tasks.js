07a79c9d643b3f5d6fe2c9128ab0d2e2cdacc8ab
Ugo Otuonye <ugoneman@gmail.com>
2019-08-27 05:44:13 +0000

    Lesson20 Add a Strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,9 @@ $(function() {
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" +
       checkedStatus +
       '><label>' +
@@ -28,7 +30,13 @@ $(function() {
       task: {
         done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+      
+    } );
   }
 
   $.get("/tasks").success( function( data ) {
@@ -44,6 +52,7 @@ $(function() {
 
   });
 
+
   $('#new-form').submit(function(event) {
     event.preventDefault();
     var textbox = $('.new-todo');
@@ -57,9 +66,7 @@ $(function() {
       var ulTodos = $('.todo-list');
       ulTodos.append(htmlString);
       $('.toggle').click(toggleTask);
-      $('.new-todo').val('');
     });
   });
 
 });
-
